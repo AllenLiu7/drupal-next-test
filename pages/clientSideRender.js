d01f56939b4bafd91a863199580f50901a6002b2
Allen Liu <allen6qi@gmail.com>
2022-06-22 09:18:09 +0800

    added cards

--- pages/clientSideRender.js
+++ pages/clientSideRender.js
@@ -1,49 +1,50 @@
 import { useState, useEffect } from 'react';
+import Image from 'next/image';
 import { drupal } from '../lib/drupal';
 import { getParams } from '../lib/getParam';
+import { toBaseUrl } from '../lib/urlBuilder';
 
 function CSG() {
-    const [data, setData] = useState();
+    const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
-            setIsLoading(true)
+            setIsLoading(true);
 
-            try{
-              const articles = await drupal.getResourceCollection(
-                  'node--article',
-                  {
-                      params: getParams()
-                          .addInclude([
-                              'field_image.uid',
-                              'field_article_news.field_media.field_media_image',
-                              'field_opinion_list',
-                          ])
-                          .addFields('node--article', [
-                              'title',
-                              'created',
-                              'field_image',
-                              'field_article_news',
-                              'field_opinion_list',
-                          ])
-                          .addFields('node--news', [
-                              'title',
-                              'body',
-                              'field_media',
-                          ])
-                          .addFields('media--image', ['thumbnail'])
-                          .addFields('file--file', ['uri'])
-                          .getQueryObject(),
-                  }           
-                  );
+            try {
+                const articles = await drupal.getResourceCollection(
+                    'node--article',
+                    {
+                        params: getParams()
+                            .addInclude([
+                                'field_image.uid',
+                                'field_article_news.field_media.field_media_image',
+                                'field_opinion_list',
+                            ])
+                            .addFields('node--article', [
+                                'title',
+                                'created',
+                                'field_image',
+                                'field_article_news',
+                                'field_opinion_list',
+                            ])
+                            .addFields('node--news', [
+                                'title',
+                                'body',
+                                'field_media',
+                            ])
+                            .addFields('media--image', ['thumbnail'])
+                            .addFields('file--file', ['uri'])
+                            .getQueryObject(),
+                    }
+                );
 
-                  setData(articles);
-                  //console.log(articles)
-                 
-          } catch (error) {
-            console.log(error)
-          }
+                setData(articles);
+                //console.log(articles)
+            } catch (error) {
+                console.log(error);
+            }
             setIsLoading(false);
         };
 
@@ -55,9 +56,46 @@ function CSG() {
             {isLoading ? (
                 <div>Loading ...</div>
             ) : (
-                <div>
-                    <pre>{JSON.stringify(data, undefined, 2)}</pre>
-                </div>
+                <>
+                    <div className='d-flex flex-wrap container'>
+                        {data.map((node) => {
+                            return (
+                                // <div key={node.field_image.id} className="m-3 col-3 flex-wrap">
+                                //   <Image  src={toBaseUrl(node.field_image.uri.url)} width={200} height={200} alt="image" class="card-img-top"/>
+                                // </div>
+                                <div
+                                    className='card col-3 m-2'
+                                    key={node.field_image.id}
+                                    style={{ width: '18rem' }}
+                                >
+                                    <Image
+                                        src={toBaseUrl(
+                                            node.field_image.uri.url
+                                        )}
+                                        width={200}
+                                        height={200}
+                                        alt='image'
+                                        className='card-img-top'
+                                    />
+                                    <div className='card-body'>
+                                        <h5 className='card-title'>
+                                            {node.title}
+                                        </h5>
+                                        <p className='card-text'>
+                                            node id: {node.id}
+                                        </p>
+                                        <a href='#' className='btn btn-primary'>
+                                            Go somewhere
+                                        </a>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                    <div>
+                        <pre>{JSON.stringify(data, undefined, 2)}</pre>
+                    </div>
+                </>
             )}
         </>
     );
--- pages/staticSideRender.js
+++ pages/staticSideRender.js
@@ -1,45 +1,78 @@
-import Image from 'next/image'
-import { drupal } from "../lib/drupal"
-import {getParams} from "../lib/getParam"
-import {toBaseUrl} from "../lib/urlBuilder"
+import Image from 'next/image';
+import { drupal } from '../lib/drupal';
+import { getParams } from '../lib/getParam';
+import { toBaseUrl } from '../lib/urlBuilder';
 
-function SSG({articles}) {
-  console.log(process.env.NEXT_PUBLIC_DRUPAL_BASE_URL)
-  return (
-    <>
-      <h2>Images</h2>
-      {articles.map(Node => {
-        return (
-          <div key={Node.field_image.id}>
-            <Image  src={toBaseUrl(Node.field_image.uri.url)} width={200} height={200} alt="image"/>
-          </div>
-        ) 
-      })}
-      
-      <pre>{JSON.stringify(articles, undefined, 2)}</pre>
-    </>
-  )
+function SSG({ articles }) {
+    console.log(process.env.NEXT_PUBLIC_DRUPAL_BASE_URL);
+    return (
+        <>
+            <div className='d-flex flex-wrap container'>
+                {articles.map((node) => {
+                    return (
+                        // <div key={node.field_image.id} className="m-3 col-3 flex-wrap">
+                        //   <Image  src={toBaseUrl(node.field_image.uri.url)} width={200} height={200} alt="image" class="card-img-top"/>
+                        // </div>
+                        <div
+                            className='card col-3 m-2'
+                            key={node.field_image.id}
+                            style={{width: '18rem'}}
+                        >
+                            <Image
+                                src={toBaseUrl(node.field_image.uri.url)}
+                                width={200}
+                                height={200}
+                                alt='image'
+                                className='card-img-top'
+                            />
+                            <div className='card-body'>
+                                <h5 className='card-title'>{node.title}</h5>
+                                <p className='card-text'>
+                                    node id: {node.id}
+                                </p>
+                                <a href='#' className='btn btn-primary'>
+                                    Go somewhere
+                                </a>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+
+            <pre>{JSON.stringify(articles, undefined, 2)}</pre>
+        </>
+    );
 }
 
 // This function gets called at build time
 export async function getStaticProps() {
-  const articles = await drupal.getResourceCollection("node--article", {
-    params: getParams()
-      .addInclude(["field_image.uid", "field_article_news.field_media.field_media_image", "field_opinion_list"])
-      .addFields('node--article', ["title","created","field_image","field_article_news", "field_opinion_list"])
-      .addFields("node--news", ["title", "body", "field_media"])
-      .addFields("media--image", ["thumbnail"])
-      .addFields("file--file", ["uri"])
-      .getQueryObject(),
-  })
+    const articles = await drupal.getResourceCollection('node--article', {
+        params: getParams()
+            .addInclude([
+                'field_image.uid',
+                'field_article_news.field_media.field_media_image',
+                'field_opinion_list',
+            ])
+            .addFields('node--article', [
+                'title',
+                'created',
+                'field_image',
+                'field_article_news',
+                'field_opinion_list',
+            ])
+            .addFields('node--news', ['title', 'body', 'field_media'])
+            .addFields('media--image', ['thumbnail'])
+            .addFields('file--file', ['uri'])
+            .getQueryObject(),
+    });
 
-  // By returning { props: { posts } }, the Blog component
-  // will receive `posts` as a prop at build time
-  return {
-    props: {
-      articles,
-    },
-  }
+    // By returning { props: { posts } }, the Blog component
+    // will receive `posts` as a prop at build time
+    return {
+        props: {
+            articles,
+        },
+    };
 }
 
-export default SSG
+export default SSG;
